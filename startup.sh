3ccf675ad25aecc0c8f65c0f780b472ad31c1e0f
LocNguyen2000 <lolkun2000@gmail.com>
2023-03-31 21:47:35 +0700

    something

--- startup.sh
+++ startup.sh
@@ -1,13 +1,15 @@
 # install docker
 sudo yum update
 sudo yum search docker
-sudo yum install docker
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+y | sudo yum install docker
+sudo systemctl restart docker
+
+# copy env file
+cp .env.example .env
 
 # docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # run compose
-docker-compose up -d
+sudo docker-compose up -d
