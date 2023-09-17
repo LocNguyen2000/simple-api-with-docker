cf0f3df3d9d83757cfd43d643d01974ba024ab31
LocNguyen2000 <lolkun2000@gmail.com>
2023-09-17 21:30:48 +0700

    trigger deploy

--- DEPLOYMENT.md
+++ DEPLOYMENT.md
@@ -5,10 +5,10 @@ Requirements: Github Actions
 ## 1. Github -> EC2
 
 - 1.1. Github push code to EC2, copy .ENV from Github Action
-- 1.2. Download docker on EC2 by shell script, run Docker build image
+- 1.2. Download docker on EC2 by shell script, run Docker build image from startup.sh
 
 ## 2. Github -> Dockerhub -> EC2
 
 - 2.1. Github push code to image in Dockerhub
-- 2.2. Access EC2> download image & copy .ENV to image source code
+- 2.2. Access EC2 > download image & copy .ENV to image source code
 - 2.3. Run image interactively
--- startup.sh
+++ startup.sh
@@ -1,9 +1,19 @@
-# install docker
-sudo yum update
-sudo yum search docker
-y | sudo yum install docker
-sleep 20
-sudo systemctl restart docker
+# install docker on Linux EC2
+if [ ! "$(docker --version)" ]; then
+    sudo yum update
+    sudo yum search docker
+    sudo yum -y install docker 
 
+    # sudo usermod -a -G docker ec2-user
+    # id ec2-user
+    # # Reload a Linux user's group assignments to docker w/o logout
+    # newgrp docker
+    sudo systemctl enable docker
+    echo "Docker installed successfully"
+else
+    echo "Docker already installed"
+fi
 
-
+# 1, docker build --tag nhlochub/simple-api-with-docker .
+# 2, docker image pull nhlochub/simple-api-with-docker
+#
